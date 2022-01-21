9ee43410159781b4b644ddb7297db1e541ee2a81
Sarvesh Patil <sarvesh2902@gmail.com>
2022-01-21 20:29:18 +0530

    Add python projects

---
+++ DataTypes/main.py
@@ -0,0 +1,44 @@
+# Numeric Data Type
+# Integer
+num = 100
+print(num)
+print("Data Type of variable num is", type(num))
+
+# Float
+fnum = 34.45
+print(fnum)
+print("Data Type of variable fnum is", type(fnum))
+
+# Complex Number
+cnum = 3 + 4j
+print(cnum)
+print("Data Type of variable cnum is", type(cnum))
+
+# String
+s = "This is a String"
+print(s)
+print(type(s))
+
+# Tuple
+t1 = (1, 2, 3, 4, 5)
+print(t1)
+t2 = (12, "Sarvesh", 34, "Patil")
+print(t2)
+t3 = (12, "Sarvesh", 34, "Patil")
+print(t3[2])
+
+# List
+lis1 = [1, 2, 3, 4, 5]
+print(lis1)
+lis2 = ["Apple", "Orange", "Banana"]
+print(lis2)
+lis3 = [20, "Sarvesh", 15, "BeginnersBook"]
+print("Element at index 3 is:", lis3[3])
+
+# Dictionary
+dict = {1: "Sarvesh", "lastname": "Patil", "age": 31}
+print(dict[1])
+
+# Set
+myset = {"Hi", 2, "Bye", "Hello World"}
+print(myset)
---
+++ Operators/main.py
@@ -0,0 +1,39 @@
+# Arithmetic Operators
+x = 5
+y = 2
+print(x - y)
+print(x + y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+# Assignment Operators
+x = 5
+print(x)
+x = 5
+x += 3
+print(x)
+x = 5
+x -= 3
+print(x)
+x = 5
+x *= 3
+print(x)
+x = 5
+x /= 3
+print(x)
+x = 5
+x = x % 3
+print(x)
+x = 5
+x //= 3
+print(x)
+x = 5
+x **= 3
+print(x)
+
+# Membership Operators
+x = ["apple", "banana"]
+print("banana" in x)
